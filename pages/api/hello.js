9862e4340439e2377cd5872f10889446e5643a50
Eduardo Nunes <esnunes@gmail.com>
2021-10-28 11:39:46 -0300

    testing prisma query

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,6 +3,6 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient({ log: ['query'] })
 
 export default async function handler(req, res) {
-  await prisma.user.findUnique({ where: { id: 1 } })
+  await prisma.user.findUnique({ where: { id: '1' } })
   res.status(200).json({ name: 'hello world' })
 }
