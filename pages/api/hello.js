c7b36ff49d5b423106474202875549392061927b
Eduardo Nunes <esnunes@gmail.com>
2021-10-28 00:43:27 -0300

    Add basic prisma user model

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,8 @@
-export default function handler(req, res) {
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({ log: ['query'] })
+
+export default async function handler(req, res) {
+  await prisma.user.findUnique({ where: { id: 1 } })
   res.status(200).json({ name: 'hello world' })
 }
